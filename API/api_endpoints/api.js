d5422eecacda503fd3a3bb5934f805fbf723e8d2
Ayooluwa <olifedayo94@gmail.com>
2018-11-09 07:37:00 +0100

    creating the data structure and the testing environment

---
+++ API/api_endpoints/api.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+
+/* eslint-disable no-unused-vars */
+
+
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+// create a non persistence database for parcels
+
+const parcels = [
+
+];
+
+// create a non persistence database for users
+const users = [
+
+];
+
+module.exports = port;
---
+++ API/spec/apitest_spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-unresolved */
+const request = require('request');
+
+describe('server', () => {
+  beforeAll(() => {
+    const server = require('./../api_endpoints/api.js');
+  });
+});
