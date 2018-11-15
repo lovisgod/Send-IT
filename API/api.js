dfe096dae899f71a5f450c0ed33e8fe89aaab450
Ayooluwa <olifedayo94@gmail.com>
2018-11-15 07:42:46 +0100

    modularizing the project

--- API/api.js
+++ API/api.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { join } from 'path';
+import * as checks from './check';
+import * as models from './models';
+
 
 const app = express();
 app.use(express.json());
@@ -10,45 +13,24 @@ app.get('/', (req, res) => {
   res.sendFile(join(`${__dirname}/../index.html`));
 });
 
-// create a non persistence database for parcels
-
-const parcels = [
-
-];
-
-// create a non persistence database for users
-const users = [
-
-];
-
 // eslint-disable-next-line prefer-destructuring
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log('listening on port 8000....... please wait');
 });
 
-const getUserParcels = (userId) => {
-  // eslint-disable-next-line no-use-before-define
-  const getid = parcels.find(c => c.userid === userId);
-  console.log(getid);
-  if (!getid) {
-    return 'no parcels for user';
-  }
-  return getid;
-};
-
 app.post('/api/v1/users', (req, res) => {
   const user = {
     userid: req.body.userid,
     name: req.body.name,
   };
-  users.push(user);
+  models.users.push(user);
   res.status(200).send(user);
 });
 
 app.post('/api/v1/parcels', (req, res) => {
   const parcel = {
-    parcelid: parcels.length + 1,
+    parcelid: models.parcels.length + 1,
     name: req.body.name,
     reciever: req.body.reciever,
     to: req.body.to,
@@ -56,17 +38,17 @@ app.post('/api/v1/parcels', (req, res) => {
     userid: req.body.userid,
     status: req.body.status,
   };
-  parcels.push(parcel);
+  models.parcels.push(parcel);
   res.status(200).send(parcel);
 });
 
 app.get('/api/v1/parcels', (req, res) => {
-  res.status(200).send(parcels);
+  res.status(200).send(models.parcels);
 });
 
 app.get('/api/v1/parcels/:parcelid', (req, res) => {
   // eslint-disable-next-line radix
-  const getparcels = parcels.find(c => c.parcelid === parseInt(req.params.parcelid));
+  const getparcels = models.parcels.find(c => c.parcelid === parseInt(req.params.parcelid));
 
   if (!getparcels) {
     res.status(400).send('the parcel with the giving id is not available');
@@ -76,8 +58,8 @@ app.get('/api/v1/parcels/:parcelid', (req, res) => {
 });
 
 app.get('/api/v1/users/:userid/parcels', (req, res) => {
-  const userParcels = getUserParcels(req.params.userid);
-  const getuser = users.find(c => c.userid === req.params.userid);
+  const userParcels = checks.getUserParcels(req.params.userid);
+  const getuser = models.users.find(c => c.userid === req.params.userid);
   if (!getuser) {
     res.status(404).send('the user with that id is not available');
   } else {
@@ -91,7 +73,7 @@ app.get('/api/v1/users/:userid/parcels', (req, res) => {
 
 app.put('/api/v1/parcels/:parcelid/cancel', (req, res) => {
   // eslint-disable-next-line radix
-  const parcelToCancel = parcels.find(c => c.parcelid === parseInt(req.params.parcelid));
+  const parcelToCancel = models.parcels.find(c => c.parcelid === parseInt(req.params.parcelid));
   if (!parcelToCancel) {
     res.status(400).send('Nothing to cancel');
   } else {
