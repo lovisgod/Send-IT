89cb3253a273f6d4d75e303b26726f9879b4d6e5
lovisgod <olifedayo94@gmail.com>
2018-11-19 05:52:10 +0100

    ch-validator-implement

--- API/validator/validator.js
+++ API/validator/validator.js
@@ -4,12 +4,12 @@ const joi = require('joi');
 
 export const validateparcels = (parcel) => {
   const schema = {
-    Name: joi.string().min(8).max(30).required(),
-    Pickup: joi.string().min(8).max(50).required(),
-    Destination: joi.string().min(8).max(50).required(),
-    Reciever: joi.string().min(8).max(30).required(),
-    userid: joi.string().min(3).max(30).required(),
-    RecieverMail: joi.string().min(8).max(30).required(),
+    Name: joi.string().min(8).max(255).required(),
+    Pickup: joi.string().min(8).max(255).required(),
+    Destination: joi.string().min(8).max(255).required(),
+    Reciever: joi.string().min(8).max(255).required(),
+    userid: joi.string().min(3).max(255).required(),
+    RecieverMail: joi.string().min(8).max(255).required(),
     Weight: joi.number().required(),
     status: 'In-Transit',
   };
